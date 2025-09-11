e8d47162f1139b88e07d312fce720a8fe189fe8f
Nournouas <171356831+Nournouas@users.noreply.github.com>
2025-09-11 15:16:22 +0100

    fix: organised modules

--- src/gameSupport.js
+++ src/gameSupport.js
@@ -1,3 +1,6 @@
+import { updateArenaUI, setBoardNames } from "./DOM";
+import { Player, ComputerPlayer} from "./Player";
+
 //checks if the coordinate is within the bound (0 to 9)
 function inBounds(x, y){
     return x >= 0 && y >= 0 && x <= 9 && y <= 9;
@@ -75,5 +78,45 @@ function getFullShipCoords(arena, ship, originalCoord){
     }
 
     return false
+}
+
+export function attackBlock(blockUI, player) {
+    const playerBoard = player.gameBoard;
+    
+    // Parse coordinates from block ID
+    const [x, y] = blockUI
+        .getAttribute("id")
+        .split(",")
+        .map(item => parseInt(item, 10));
+
+    // Perform attack
+    playerBoard.receiveAttack([x, y]);
+    
+    if (playerBoard.coordinates[x][y].ship.sunk === true){
+        let shipCoords = playerBoard.coordinates[x][y].ship.coordinate;
+
+        for (let [sx, sy] of shipCoords){
+            let blockId = `${sx},${sy}`;
+            let block = document.querySelector(`#${CSS.escape(player.name)} > #${CSS.escape(blockId)}`);
+            block.classList.add("destroyed");
+        }
+        
+    }
+    // Update UI
+    updateArenaUI(player, `#${player.name}`);
+}
+
+export function startGame(player1, player2){
+        let player = new Player(player1);
+        let computer = new ComputerPlayer(player2);
+        setBoardNames(player, computer);
+
+        randomlyPositionShips(player);
+        randomlyPositionShips(computer);
+        updateArenaUI(player, `#${player1}`);
+        updateArenaUI(computer, `#${player2}`);
+
+        return {player1: player, player2: computer}
+
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import "./styles.css";
 import "./cssReset.css"
 import { ComputerPlayer, Player } from "./Player";
-import { addBlockEventListener, initUI, updateArenaUI, setBoardNames} from "./DOM";
-import { randomlyPositionShips } from "./gameSupport";
+import { addBlockEventListener, initUI} from "./DOM";
+import { randomlyPositionShips, attackBlock, startGame} from "./gameSupport";
+
 
 
 //(function (){  
     initUI();
     let players = startGame("player", "computer");
-    let turn = players.player1;
+
     addBlockEventListener(players.player1, attackBlock)
     addBlockEventListener(players.player2, attackBlock)
 
@@ -16,46 +17,7 @@ import { randomlyPositionShips } from "./gameSupport";
         
     }
 
-    function attackBlock(blockUI, player) {
-        const playerBoard = player.gameBoard;
-        
-        // Parse coordinates from block ID
-        const [x, y] = blockUI
-            .getAttribute("id")
-            .split(",")
-            .map(item => parseInt(item, 10));
 
-        // Perform attack
-        playerBoard.receiveAttack([x, y]);
-        
-        if (playerBoard.coordinates[x][y].ship.sunk === true){
-            let shipCoords = playerBoard.coordinates[x][y].ship.coordinate;
-
-            for (let [sx, sy] of shipCoords){
-                let blockId = `${sx},${sy}`;
-                let block = document.querySelector(`#${CSS.escape(player.name)} > #${CSS.escape(blockId)}`);
-                block.classList.add("destroyed");
-            }
-            
-        }
-        // Update UI
-        updateArenaUI(player, `#${player.name}`);
-    }
-
-
-    export function startGame(player1, player2){
-        let player = new Player(player1);
-        let computer = new ComputerPlayer(player2);
-        setBoardNames(player, computer);
-
-        randomlyPositionShips(player);
-        randomlyPositionShips(computer);
-        updateArenaUI(player, `#${player1}`);
-        updateArenaUI(computer, `#${player2}`);
-
-        return {player1: player, player2: computer}
-
-    }
 
 
 
